ffe967315789b234c3bdf6f61fb84e36d327b236
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2025-06-18 15:35:15 +0200

    Contacto Form solved

--- contacto.js
+++ contacto.js
@@ -1,4 +1,4 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
+function form_send(e) {
     e.preventDefault(); 
 
     let nombre = document.getElementById("nombre");
@@ -9,25 +9,33 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
 
     if (nombre.value == "") {
         nombre.style.border = "2px solid red";
-        valid = false;
+        nombre.style.color = "red";
+        valid=false;
     } else {
         nombre.style.border = "2px solid green";
+        nombre.style.color = "green";
     }
 
     let format = /(\<|^)[\w\d._%+-]+@(?:[\w\d-]+\.)+(\w{2,})(\>|$)/i;;
 
     if (!format.test(email.value.trim())) {
         email.style.border = "2px solid red";
-        valid = false;
+        email.style.color = "red";
+        valid=false;
     } else {
-        valid=true;
         email.style.border = "2px solid green";
+        email.style.color = "green";
     }
 
     if (mensaje.value == "") {
         mensaje.style.border = "2px solid red";
-        valid = false;
+        mensaje.style.color = "red";
+        valid=false;
     } else {
         mensaje.style.border = "2px solid green";
+        mensaje.style.color = "green";
     }
-});
+    if(valid){
+        document.getElementById("contactForm").submit();
+    }
+};
